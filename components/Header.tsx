bec94f45ad59652317cdbfbef2c849008f0294a5
sain1aurent <crismagrata@gmail.com>
2023-04-12 18:23:47 +0800

    style: added icons on overview component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
-//react-icons
-import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { Link } from "react-scroll";
 //context
 import { useAppContext } from "@/context/StateContext";
+//react-icons
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { IoDocumentAttachOutline } from "react-icons/io5";
+import { Link } from "react-scroll";
+
 const Header = () => {
   const [activeLink, setActiveLink] = useState<
     "home" | "about" | "contact" | "projects"
@@ -55,41 +57,41 @@ const Header = () => {
     };
   }, [sectionRefs]);
 
-  const handleLinkClick = (
-    linkId: "home" | "about" | "contact" | "projects"
-  ): void => {
-    setActiveLink(linkId);
+  //added border color white when clicked
+  // const handleLinkClick = (
+  //   linkId: "home" | "about" | "contact" | "projects"
+  // ): void => {
+  //   setActiveLink(linkId);
+  //   switch (linkId) {
+  //     case "home":
+  //       setBackgroundColor("border-white");
+  //       break;
+  //     case "about":
+  //       setBackgroundColor("border-white");
 
-    switch (linkId) {
-      case "home":
-        setBackgroundColor("bg-white text-black");
-        break;
-      case "about":
-        setBackgroundColor("bg-white text-black");
-
-        break;
-      case "contact":
-        setBackgroundColor("bg-white text-black");
-        break;
-      case "projects":
-        setBackgroundColor("bg-white text-black");
-        break;
-      default:
-        setBackgroundColor("");
-        break;
-    }
-  };
+  //       break;
+  //     case "contact":
+  //       setBackgroundColor("border-white");
+  //       break;
+  //     case "projects":
+  //       setBackgroundColor("border-white");
+  //       break;
+  //     default:
+  //       setBackgroundColor("");
+  //       break;
+  //   }
+  // };
   return (
     <div className="fixed top-0 w-full">
       <div className="flex  mx-auto max-w-[72rem] h-28 items-center justify-center px-4">
         <nav>
-          <ul className="flex gap-5 items-center border-2 border-white rounded-full p-1 bg-black ">
+          <ul className="flex gap-2 items-center border-2 border-white rounded-full p-1 bg-black ">
             <Link to="home" smooth={true}>
               <li
                 // onClick={() => handleLinkClick("home")}
                 className={`${
                   activeLink === "home" ? backgroundColor : ""
-                } cursor-pointer rounded-full  p-2 px-3 duration-500 border-2 border-black hover:border-white`}
+                } cursor-pointer rounded-full  p-2 px-3 duration-500 border-2 border-transparent hover:border-white`}
               >
                 Home
               </li>
@@ -125,17 +127,33 @@ const Header = () => {
                 Contact
               </li>
             </Link>
-            <li className="text-lg ml-3 cursor-pointer ">
+            <li className="flex gap-4 px-2">
               <a
+                id="linkedin"
                 href="https://www.linkedin.com/in/crismagrata/"
                 target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-[#0073B2] duration-500 text-lg  cursor-pointer"
               >
-                <BsLinkedin className="hover:text-[#0073B2] duration-500" />
+                <BsLinkedin />
               </a>
-            </li>
-            <li className="cursor-pointer text-lg p-2">
-              <a href="https://github.com/magratacris" target="_blank">
-                <BsGithub className="hover:text-gray-500 duration-500" />
+              <a
+                id="github"
+                href="https://github.com/magratacris"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-500 duration-500 cursor-pointer text-lg"
+              >
+                <BsGithub />
+              </a>
+              <a
+                id="resume"
+                href="https://github.com/magratacris"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="hover:text-gray-500 duration-500 cursor-pointer text-lg"
+              >
+                <IoDocumentAttachOutline />
               </a>
             </li>
           </ul>
--- components/Overview.tsx
+++ components/Overview.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+//next
+import Image from "next/image";
 const Overview = () => {
   return (
     <div className="max-w-[72rem] mx-auto h-[30rem] flex gap-3 items-center px-4 py-24 ">
@@ -8,15 +9,32 @@ const Overview = () => {
       </div>
 
       <div className="flex-1 flex flex-col gap-4 justify-center items-center">
-        <div className="flex gap-4 ">
-          <div className="border-2 border-white p-5 rounded-2xl">logo</div>
+        <div className="flex gap-4 items-center">
+          <div className="border-2 border-white p-5 rounded-2xl">
+            <Image
+              src="/images/overview-grad.png"
+              alt=""
+              width={512}
+              height={512}
+              className="h-16 w-16"
+            />
+          </div>
           <div className="w-80">
             <h1 className="text-xl">BS Computer Engineering</h1>
             <p className="text-sm text-gray-500"> August 2022</p>
           </div>
         </div>
-        <div className="flex gap-4">
-          <div className="border-2 border-white p-5 rounded-2xl">logo</div>
+        <div className="flex gap-4 items-center">
+          <div className="border-2 border-white p-5 rounded-2xl">
+            {" "}
+            <Image
+              src="/images/overview-courses.png"
+              alt=""
+              width={512}
+              height={512}
+              className="h-16 w-16"
+            />
+          </div>
           <div className="w-80">
             <h1 className="text-xl">Online Courses</h1>
             <p className="text-sm text-gray-500">
