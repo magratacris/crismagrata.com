78ae5c9e285f08fa895f17054293a7121300c1fe
sain1aurent <crismagrata@gmail.com>
2023-04-19 20:41:52 +0800

    feat: add vercel analytics package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+//react-context
 import { AppContext } from "@/context/StateContext";
+//component
 import Layout from "@/components/Layout";
+//vercel
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContext>
       <Layout>
         <Component {...pageProps} />
+
+        <Analytics />
       </Layout>
     </AppContext>
   );
